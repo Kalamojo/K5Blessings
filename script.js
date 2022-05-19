04f21e4714dbfdea76557b1f937a97bbcde5af58
Kalamojo <kalabiprof@gmail.com>
2022-05-18 20:14:45 -0500

    Navigation active highlighting

--- script.js
+++ script.js
@@ -16,16 +16,20 @@ document.addEventListener("scroll", function() {
     getProgress();
 })
 function getProgress() {					
-    let topPos = document.body.scrollTop;
+    let topPos = window.pageYOffset;
+    let preHeight = 0;
     // Alternatively, you can use document.body.scrollTop || document.documentElement.scrollTop;
 
     for(let i = 0; i < sections.length; i++) {
-        if(sections[i].getBoundingClientRect().top <= topPos + 250 && sections[i].getBoundingClientRect().top + sections[i].getBoundingClientRect().height > topPos) {
+        let currentPos = sections[i].getBoundingClientRect().top;
+        if(currentPos <= 200) {
             document.querySelector(".active").className = document.querySelector(".active").className.replace(" active", "");
             btns[i].className += " active";
-        } else {
-            btns[i].className.replace(" active", "");
         }
+        if(window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            document.querySelector(".active").className = document.querySelector(".active").className.replace(" active", "");
+            btns[4].className += " active";
+        }        
     };
     // scrollHeight is the measurement of the element's entire content, whether all the content is visible or not
     // clientHeight is the inner height of the element, including padding
